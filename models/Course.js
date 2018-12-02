a9696faf08d51687b1ee8020fef53413a46eb062
AsaGayle <azmatch.gayle@gmail.com>
2018-12-01 22:25:22 -0500

    Removed dayTime schema due to no longer needing it

--- models/Course.js
+++ models/Course.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//Create Schema 
-const day_TimeSchema = new Schema({
-    type:[String]
-});
-
 const CourseSchema = new Schema({
     _id:{
         type: String,
